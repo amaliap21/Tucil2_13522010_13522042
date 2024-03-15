fcbf92984835fd297bee472478ee97aed589c707
Amalia Putri <amaliaputriii2104@gmail.com>
2024-03-15 15:04:12 +0700

    feat: fix all test output saving

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import time
 from BruteForce import bezier_brute_force
 from DivideAndConquer import bezier_divide_and_conquer
 import os
+import glob
 import shutil
 
 ##### ALGORITMA UTAMA #####
@@ -18,11 +19,10 @@ def choose_algorithm():
 # Gambar Kurva Bezier
 def draw_bezier():
     global control_points
+    global alg_choice
+
     plt.clf()
     plt.plot(*zip(*control_points), marker='o', color='peachpuff', linestyle='-', label="Control Points")
-
-    # Pilih Algoritma
-    alg_choice = choose_algorithm()
 
     # Waktu Mulai
     start = time.time()
@@ -40,9 +40,6 @@ def draw_bezier():
         BezierPoints, MiddlePoints = bezier_divide_and_conquer(BezierPoints, 0, 5)
         plt.plot(*zip(*MiddlePoints), marker = 'o', color = 'pink', linestyle = 'dashed', label="Pass 2 Middle Points")
         plt.plot(*zip(*BezierPoints), marker = 'o', color='crimson', linestyle = 'solid', label="Pass 2 Bezier Curve\n(Divide and Conquer)")
-    else:
-        print("Invalid algorithm choice. Please enter 'BF' for Brute Force or 'DAC' for Divide and Conquer.")
-        return
 
     # Waktu Eksekusi dan Tampilkan Hasil
     execution_time = (time.time() - start) * 1000
@@ -54,13 +51,39 @@ def draw_bezier():
 # Gambar Titik
 def add_points():
     global control_points
+    global alg_choice
+    global num_of_folders
+    
+    # Ensure num_of_folders is up-to-date
+    update_num_of_folders()
+    folder_path = f'./test/Output/{num_of_folders}'
+    
+    # Make a folder to save the output images
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    
+    # Pilih Algoritma
+    alg_choice = choose_algorithm()
+
+    # Validasi Input Algoritma
+    while (alg_choice != 'BF' and alg_choice != 'DAC'):
+        simpledialog.askstring("Choose Algorithm", "Enter 'BF' for Brute Force or 'DAC' for Divide and Conquer:", parent=root)
+        alg_choice = choose_algorithm()
+
     control_points = []
     num_of_points = simpledialog.askinteger("Input", "How many points?", parent=root)
     if num_of_points:
-        for _ in range(num_of_points):
+        for i in range(num_of_points):
             x, y = simpledialog.askstring("Input", "Enter point (x,y):", parent=root).split(',')
             control_points.append((float(x), float(y)))
-    draw_bezier()
+            draw_bezier()
+            
+            # Save the figure
+            if (i != num_of_points - 1):
+                plt.savefig(f'./test/Output/{num_of_folders}/BezierCurve_{alg_choice}({i+1}).png')
+            else:
+                plt.savefig(f'./test/Output/{num_of_folders}/BezierCurve_{alg_choice}(final result).png')
+                num_of_folders += 1
 
 # Judul dan Tampilan GUI
 root = tk.Tk()
@@ -79,11 +102,28 @@ btn_add_points.pack(side=tk.LEFT, pady=20, padx=10)
 root.mainloop()
 
 
+##### ALGORITMA UPDATE DIRECTORY #####
+def update_num_of_folders():
+    global num_of_folders
+    output_path = './test/Output/'
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+        num_of_folders = 1
+    else:
+        # List existing directories and find the maximum existing folder number
+        folders = glob.glob(output_path + '*/')
+        max_num = 0
+        for folder in folders:
+            folder_name = os.path.basename(os.path.dirname(folder))  
+            if folder_name.isdigit():
+                max_num = max(max_num, int(folder_name))
+        num_of_folders = max_num + 1  
+
 ##### ALGORITMA PEMINDAHAN CACHE #####
 src_pycache = './src/__pycache__'
 dest_bin = './bin'
 
-# Function to move the __pycache__ to bin
+# Pindahkan isi __pycache__ ke bin
 def move_pycache_to_bin():
     # Check if __pycache__ exists in the src directory
     if os.path.exists(src_pycache):
